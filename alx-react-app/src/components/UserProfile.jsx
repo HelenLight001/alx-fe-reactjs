2c472dd24dc0427fbcca27636b24428ce21000fb
HelenLight001 <helenemmanuel704@gmail.com>
2025-07-10 00:52:09 +0100

    Added React and some components

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const UserProfile = (props) => {
+  return (
+    <div
+      style={{
+        border: "2px solid rgb(54, 140, 197)",
+        borderRadius: "8px",
+        padding: "16px",
+        maxWidth: "300px",
+        margin: "16px auto",
+        textAlign: "center",
+      }}
+    >
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>{props.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
