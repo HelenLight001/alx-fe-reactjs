47c0c4ad925a668827ec00e21424912d4a1b2835
HelenLight001 <helenemmanuel704@gmail.com>
2025-07-10 00:57:24 +0100

    updated UserProfile

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -2,19 +2,10 @@ import React from "react";
 
 const UserProfile = (props) => {
   return (
-    <div
-      style={{
-        border: "2px solid rgb(54, 140, 197)",
-        borderRadius: "8px",
-        padding: "16px",
-        maxWidth: "300px",
-        margin: "16px auto",
-        textAlign: "center",
-      }}
-    >
+    <div>
       <h2>{props.name}</h2>
       <p>Age: {props.age}</p>
-      <p>{props.bio}</p>
+      <p>Bio: {props.bio}</p>
     </div>
   );
 };
