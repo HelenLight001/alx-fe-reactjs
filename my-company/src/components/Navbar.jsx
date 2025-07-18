d97a56cc5858915db7f99d4b1d0afee5ccdcbca7
HelenLight001 <helenemmanuel704@gmail.com>
2025-07-18 14:55:09 +0100

    Updated the Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,32 +2,36 @@ import {Link} from "react-router-dom";
 
 function Navbar() {
   const navStyle = {
-    padding: "10px",
+    padding: "10px 20px",
     backgroundColor: "#333",
     color: "#fff",
     display: "flex",
-    gap: "20px",
+    justifyContent: "space-between",
+    alignItems: "center",
   };
 
   const linkStyle = {
     color: "#fff",
     textDecoration: "none",
+    marginLeft: "15px",
   };
 
   return (
     <nav style={navStyle}>
-      <Link to="/" style={linkStyle}>
-        Home
-      </Link>
-      <Link to="/about" style={linkStyle}>
-        About
-      </Link>
-      <Link to="/services" style={linkStyle}>
-        Services
-      </Link>
-      <Link to="/contact" style={linkStyle}>
-        Contact
-      </Link>
+      <div>
+        <Link to="/" style={linkStyle}>
+          Home
+        </Link>
+        <Link to="/about" style={linkStyle}>
+          About
+        </Link>
+        <Link to="/services" style={linkStyle}>
+          Services
+        </Link>
+        <Link to="/contact" style={linkStyle}>
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 }
