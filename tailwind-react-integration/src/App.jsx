55f21f601dc5d6c7c92cb9b2672e2a5c6973a4c9
HelenLight001 <helenemmanuel704@gmail.com>
2025-08-10 13:31:52 +0100

    Added the tailwind-react-integration directory

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,16 @@
+import {useState} from "react";
+import React from "react";
+import UserProfile from "./components/UserProfile";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <UserProfile />
+      </div>
+    </>
+  );
+}
+
+export default App;
