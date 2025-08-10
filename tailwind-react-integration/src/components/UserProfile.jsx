1c98a0e0f078bb2b756b2fc37faacb0da2a80d76
HelenLight001 <helenemmanuel704@gmail.com>
2025-08-10 13:34:39 +0100

    updated the UserProfile.jsx file

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -8,7 +8,7 @@ function UserProfile() {
         className="rounded-full sm:w-24 sm:h-24 md:h-36 mx-auto md:w-36 hover:scale-110 transition-transform duration-300 ease-in-out "
       />
       <h1 className="md:text-xl  sm:text-lg text-blue-800 my-4 hover:text-blue-500">
-        John Doe
+        Helen Chinemerem
       </h1>
       <p className="text-gray-600 md:text-base sm:text-sm">
         Developer at Example Co. Loves to write code and explore new
