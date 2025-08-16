a4312d78d363b10ec67ab7ca899d06630a271b14
HelenLight001 <helenemmanuel704@gmail.com>
2025-08-16 04:33:29 +0100

    updated footer.jsx

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // Can be 'media' or 'class'
+  darkMode: "media", // Can be 'media' or 'class'
   theme: {
     extend: {},
   },
