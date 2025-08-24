74b279e99f74c751695e463d206612edf9d117ad
HelenLight001 <helenemmanuel704@gmail.com>
2025-08-24 19:19:20 +0100

    updated App.jsx  react-router-advanced directory

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,25 +1,25 @@
 // src/App.jsx
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 
 export default function App() {
   return (
-    <div>
-      <nav className="p-4 bg-gray-200 flex gap-4">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/profile">Profile</Link>
-      </nav>
+    <BrowserRouter>
+      <div>
+        <nav className="p-4 bg-gray-200 flex gap-4">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/profile">Profile</Link>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-
-        {/* Nested routes for profile */}
-        <Route path="/profile/*" element={<Profile />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile/*" element={<Profile />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
